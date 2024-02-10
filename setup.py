8a259cd0c3a802f4ecf3b88c5f020d7210eff7ca
fineplus <fineplus@proton.me>
2024-02-10 22:17:48 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='pyfineflow',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(exclude=['__pycache__']),
     description='python nodes server for fineflow',
     long_description=long_description,
